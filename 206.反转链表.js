4c49c9dc0e61d57e84f6c92abaac43c3f15e1042
Lex <zhang13pro@qq.com>
2021-10-28 18:34:59 +0800

    [206] 反转链表

---
+++ 206.反转链表.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+  let res = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return res;
+};
+// @lc code=end
