e29b1a97ab68fb7030e9db42fc987d522ecaef9a
Lex <zhang13pro@qq.com>
2021-11-01 17:24:00 +0800

    first time

---
+++ 3.无重复字符的最长子串.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  const map = {};
+  let left = 0;
+
+  let result = s.split("").reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0);
+
+  return result;
+};
+// @lc code=end
