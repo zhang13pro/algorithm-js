15e11ff8a147c5f4938162d3ea2bb55c13de9ad5
Lex <zhang13pro@qq.com>
2021-11-18 10:31:41 +0800

    121 again

--- 贪心/121.买卖股票的最佳时机.js
+++ 贪心/121.买卖股票的最佳时机.js
@@ -2,8 +2,6 @@
  * @lc app=leetcode.cn id=121 lang=javascript
  *
  * [121] 买卖股票的最佳时机
- * @Version 2.0
- * @Date November 11,2021
  */
 
 // @lc code=start
@@ -12,12 +10,12 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let max = 0,
-    min = Number.MAX_VALUE;
-  for (const money of prices) {
-    min = Math.min(min, money);
-    max = Math.max(max, money - min);
+  let lowPrice = Number.MAX_VALUE,
+    profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    lowPrice = Math.min(lowPrice, prices[i]);
+    profit = Math.max(profit, prices[i] - lowPrice);
   }
-  return max;
+  return profit;
 };
 // @lc code=end
