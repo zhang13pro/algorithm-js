ddba98de58cb7afef62a6aa90b8cae1e157d39f4
Lex <zhang13pro@qq.com>
2021-11-22 15:04:10 +0800

    [134] 加油站

---
+++ 动态规划/134.加油站.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=134 lang=javascript
+ *
+ * [134] 加油站
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function (gas, cost) {
+  let curTank = 0,
+    totalTank = 0,
+    index = 0;
+  for (let i = 0; i < gas.length; i++) {
+    curTank += gas[i] - cost[i];
+    totalTank += gas[i] - cost[i];
+    if (curTank < 0) {
+      curTank = 0;
+      index = i + 1;
+    }
+  }
+
+  return totalTank < 0 ? -1 : index;
+};
+// @lc code=end
